5d6b9ed72071f3be3991de6f62f7969150ec5ebd
Ataquex <jonas.addicks@gmx.de>
2022-06-08 18:56:31 +0200

    test commit new device

--- src/Main/main.java
+++ src/Main/main.java
@@ -6,6 +6,7 @@ import GUI.GUI;
 public class main{
 
     public static void main (String[] args){
+        System.out.println(". ./");
         Controller controller = new Controller();
         controller.startController(controller);
     }
