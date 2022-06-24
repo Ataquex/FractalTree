f4576a8f2f36fa4677ac2528a893c47d72fc8ddd
Ataquex <jonas.addicks@gmx.de>
2022-06-25 00:22:56 +0200

    clear warnings

--- src/LineBuilder/LineBuilder.java
+++ src/LineBuilder/LineBuilder.java
@@ -31,17 +31,18 @@ public class LineBuilder extends JComponent {
         graphic.setStroke(new BasicStroke(RealizationLimit));
         graphic.setColor(Color.decode("#ffffff"));
 
-        TreeRecursive(graphic, 0, 0, 0, FirstBranchLength, BranchLengthScaling, NumberBranchesPerNode, AngleBranchesPerNode, AngleMotherToDaughterBranch, RealizationLimit, randomness, RealizationLimit);
+        TreeRecursive(graphic, 0, 0, 0, FirstBranchLength, BranchLengthScaling, NumberBranchesPerNode, AngleBranchesPerNode, AngleMotherToDaughterBranch, RealizationLimit, randomness, RealizationLimit );
 
     }
 
     public void TreeRecursive(Graphics2D graphic, int x1, int y1, int x2, int y2, float scaling, int branchespernode, double anglenode, double anglemotherdaughter, int realization, int randomness, int strokewidth){
+
         graphic.setStroke(new BasicStroke(strokewidth));
         graphic.drawLine(x1, y1, x2, -y2);
         graphic.translate(x2, -y2);
 
         realization--;
-        if(realization > 0) {
+        if(realization > 0){
 
             for(int i = 0; i < branchespernode; i++) {
 
@@ -49,12 +50,12 @@ public class LineBuilder extends JComponent {
                 double randomfactorAngle = 1;
 
                 if(randomness > 0){
-                    randomfactorScaling = 1 + ((ThreadLocalRandom.current().nextDouble(0, 1 + randomness * 5) - (randomness * 3 / 2)) / 100);
-                    randomfactorAngle = 1 + ((ThreadLocalRandom.current().nextDouble(0, 1 + randomness * 10) - (randomness * 3 / 2)) / 100);
+                    randomfactorScaling = 1 + ((ThreadLocalRandom.current().nextDouble(0, 1 + randomness * 5) - (randomness * 1.5)) / 100);
+                    randomfactorAngle = 1 + ((ThreadLocalRandom.current().nextDouble(0, 1 + randomness * 10) - (randomness * 1.5)) / 100);
                 }
 
                 double saveangle = ((anglemotherdaughter * PI - PI) + (i * (anglenode * PI))) * randomfactorAngle;
-                graphic.rotate(-(((anglemotherdaughter * PI - PI) + (i * (anglenode * PI))) * randomfactorAngle));
+                graphic.rotate(-(saveangle));
 
                 TreeRecursive(graphic, x1, y1, x2, (int) (y2 * (scaling * randomfactorScaling)), scaling, branchespernode, anglenode, anglemotherdaughter, realization, randomness, (strokewidth - 1));
                 graphic.translate(0, (int) (y2 * (scaling * randomfactorScaling)));
